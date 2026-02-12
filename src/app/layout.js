708541a8fde284f1395e5ce169028157ef0cf004
Hammad Ali <hammadali.cse123@gmail.com>
2026-02-12 17:12:09 +0500

    Project Updated # 1

---
+++ src/app/layout.js
@@ -0,0 +1,48 @@
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import LightRays from "@/components/LightRays";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-gotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DevEvent",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased`}
+      >
+        <Navbar />
+        <div className="absolute inset-0 top-0 -z-[1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0}
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
